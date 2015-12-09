389325381b504cb4fa5ca650514836ce9c0f844f
Ryan Wong <ryan.wong@biola.edu>
2015-12-08 21:45:37 -0800

    ???

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124050449) do
+ActiveRecord::Schema.define(version: 20151209050741) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "body"
+    t.string   "username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
